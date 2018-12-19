6c90269d2aed59dd880a14111b636a49bf580b62
Alberto <alberto.galimba@gmail.com>
2018-12-19 14:47:06 +0800

    Assets/Libs

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+var consoleError = 'Something went wrong! Try again later.';
+var createMessage = {
+  title: 'Success',
+  message: 'Record successfully <strong>created</strong>'
+}
+var updateMessage = {
+  title: 'Success',
+  message: 'Record successfully <strong>updated</strong>'
+}
+var deleteMessage = {
+  title: 'Success',
+  message: 'Record successfully <strong>deleted</strong>'
+}
+var errorMessage = {
+  title: 'Error',
+  message: 'Something went wrong! <strong>Try again later.</strong>'
+}
